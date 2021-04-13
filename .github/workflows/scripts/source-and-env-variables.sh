779d00abea646fdf67e1bef7a5354c8aa1fec95b
Kenton Shade <kenton@Lenovo-Edge-15-ubuntu-20-04-lts.local>
2021-04-13 16:21:19 -0400

    troubleshooting path variables

--- .github/workflows/scripts/source-and-env-variables.sh
+++ .github/workflows/scripts/source-and-env-variables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -xv
 ## Environment
+cd
+ls -la
+cd /__w/newrelic-ruby-agent/newrelic-ruby-agent
 HOME=/root
+cat /root/.bashrc
 source /root/.bashrc
 RBENV_BIN="/root/.rbenv/bin/rbenv"
 export PATH=${RBENV_BIN}:$PATH
