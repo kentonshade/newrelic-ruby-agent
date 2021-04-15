215da9c409f969e81fe5862d4353999d69c6bcbd
Kenton Shade <kenton@Lenovo-Edge-15-ubuntu-20-04-lts.local>
2021-04-15 14:02:24 -0400

    changing command to use new gha user

--- .github/workflows/scripts/source-and-env-variables.sh
+++ .github/workflows/scripts/source-and-env-variables.sh
@@ -3,9 +3,9 @@ set -xv
 ## Environment
 whoami
 echo "$USER"
-HOME=/root
-cat /root/.bashrc
-source /root/.bashrc
-. /root/.bashrc
+# HOME=/root
+# cat /root/.bashrc
+# source /root/.bashrc
+# . /root/.bashrc
 # RBENV_BIN="/root/.rbenv/bin/rbenv"
 # export PATH=${RBENV_BIN}:$PATH
